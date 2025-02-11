b2851960f6f95c983b8aa29b35d1e1b0bef4c04f
kyomatsumoto <gpdj2001@gmail.com>
2025-02-11 14:03:17 +0900

    コメントアウトの修正

--- app/controllers/backlog_controller.rb
+++ app/controllers/backlog_controller.rb
@@ -8,7 +8,6 @@ class BacklogController < ApplicationController
 
     Rails.logger.info("Received webhook: #{json_data}")
 
-    # 必要な処理をここに実装
     # 例: json_dataの中身に応じて処理を実行
 
     render json: { message: "Webhook received successfully" }, status: :ok
