e61fb6229d8d09d81fdb558790fa965a8abe901d
Yuta Ueda <>
2025-02-11 14:14:16 +0900

    CSRF対策の修正

--- app/controllers/backlog_controller.rb
+++ app/controllers/backlog_controller.rb
@@ -1,12 +1,13 @@
 class BacklogController < ApplicationController
-  protect_from_forgery with: :null_session  # CSRF対策を無効化 (API用)
-  # skip_before_action :verify_authenticity_token, only: [ :webhook ]
+  # protect_from_forgery with: :null_session  # CSRF対策を無効化 (API用)
+  skip_before_action :verify_authenticity_token, only: [ :webhook ]
 
   def webhook
     payload = request.body.read
     json_data = JSON.parse(payload) rescue {}
 
     Rails.logger.info("Received webhook: #{json_data}")
+    Rails.logger.info("Received webhook(raw): #{payload}")
 
     # 例: json_dataの中身に応じて処理を実行
 
